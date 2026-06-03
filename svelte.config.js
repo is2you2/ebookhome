46041903b92d35f97813e101e0cf925162294c8a
liss22 <liss22@hanmail.net>
2026-06-04 00:49:18 +0900

    base_href 설정

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,9 @@ export default {
   kit: {
     adapter: adapter({
       fallback: 'index.html' // SPA fallback 필요하면
-    })
+    }),
+    paths: {
+      base: '/ebook_sample' // GitHub Pages에 배포할 때 필요
+    }
   }
 };
